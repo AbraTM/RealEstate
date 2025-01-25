0e45eecee44eabf7bb2b2c3608f8ef2b18bdb886
AbraTM <tusharmalhan2564@gmail.com>
2025-01-25 22:33:23 +0530

    Fix

--- RE_backend/util/cloudinary.js
+++ RE_backend/util/cloudinary.js
@@ -21,7 +21,7 @@ const uploadOnCloudinary = async(localFilePath) => {
         console.log("File uploaded successfully!!");
         return response
     } catch (error) {
-        fs.unlink(localFilePath)
+        await fs.unlink(localFilePath)
         return null
     }
 }
