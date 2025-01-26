87a490cd07c88c395a6fb980913786e1b7a6cf07
AbraTM <tusharmalhan2564@gmail.com>
2025-01-26 19:01:00 +0530

    fixing cors

--- RE_backend/app.js
+++ RE_backend/app.js
@@ -6,9 +6,8 @@ const app = express()
 const connectDB = require("./db/connect")
 const cors = require('cors');
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
 };
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
